2c2c8c280d9f4a25d928414195d9e241871dd7d8
Adeyemi Arimoro <arimoro71@gmail.com>
2023-03-21 08:45:24 -0500

    Adds solution to index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3 = 10;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+const max = Math.max(2, 8, 20, 15, 9);
